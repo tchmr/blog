b5fc9feb290b92792528907d15a5716717e71c3a
Yosuke <s7k315@gmail.com>
2019-05-03 10:00:48 +0900

    update_to_devise_install

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,52 @@
 class ArticlesController < ApplicationController
+  # before_action :current_article, only: [:edit, :update, :destroy]
+
+  def current_article
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all.order(updated_at: :desc)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:notice] = "記事を投稿しました"
+      redirect_to root_path
+    else
+      flash[:alert] = "エラーが発生しました"
+      render :new
+    end
+  end
+
+  def edit
+    current_article
+  end
+
+  def update
+    current_article
+    @article.update(article_params)
+    redirect_to root_path, notice: "記事を更新しました"
+  end
+
+  def destroy
+    current_article
+    @article.destroy
+    redirect_to root_path notice: "記事を削除しました"
+  end
+
+  private
+  def article_params
+    # params.permit(:title, :body)
+    params.require(:article).permit(:title, :body)
+  end
 end
